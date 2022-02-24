2ba26dd42abbb6c49a2b1af1cb8f8e25c1664be2
Andri Junaedi <73779893+andrijunaedi@users.noreply.github.com>
2022-02-24 13:58:04 +0700

    fix(ci): setup atomation semantic-release (#2)

--- release.config.js
+++ release.config.js
@@ -16,11 +16,10 @@ module.exports = {
       },
     ],
     '@semantic-release/npm',
-    '@semantic-release/github',
     [
-      '@semantic-release/git',
+      '@semantic-release/github',
       {
-        assets: ['CHANGELOG.md', 'dist/**'],
+        assets: ['dist/**/*.{js,css}', 'docs'],
         message:
           'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
